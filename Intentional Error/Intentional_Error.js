61e1ece3170fe2cbf8e680d45eb8fd6443095758
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 21:24:03 +0500

    Intentional Error:
     If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ Intentional Error/Intentional_Error.js
@@ -0,0 +1,8 @@
+"use strict";
+// received an array index error in one of your programs yet, try to make one happen
+// creating array
+let newArray = ['Nadir', 'Anjum', 'Sara'];
+// Accessing index by array
+console.log(newArray[5]);
+// correct the error
+console.log(newArray[2]);
---
+++ Intentional Error/Intentional_Error.ts
@@ -0,0 +1,9 @@
+// received an array index error in one of your programs yet, try to make one happen
+// creating array
+let newArray :string[] = ['Nadir', 'Anjum', 'Sara'];
+
+// Accessing index by array
+console.log(newArray[5]);
+
+// correct the error
+console.log(newArray[2]);
