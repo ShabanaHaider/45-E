f0779916ab765897816fd11df3688ec3ed505585
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 20:17:57 +0500

    Guest List:
     If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Guest List/Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['Ansar Burni', 'Adeeb Rizvi', 'Anwar Maqsood'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nYou are requested to become a guest in Social Sciences Conference.\n\Thank You!');
+}
---
+++ Guest List/Guest_List.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_List :string[] = ['Ansar Burni', 'Adeeb Rizvi', 'Anwar Maqsood'];
+
+for(let i =0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr. '  + Guest_List[i] + ',\n\nYou are requested to become a guest in Social Sciences Conference.\n\Thank You!')
+}
