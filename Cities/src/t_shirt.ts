3294e40f9d588dbfb46d381b29167470b9b72941
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 22:13:43 +0500

    Ordinal Numbers
    : Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
    • Store the numbers 1 through 9 in a array.
    
    • Loop through the array.
    
    • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.

---
+++ Cities/src/t_shirt.ts
@@ -0,0 +1,20 @@
+
+
+// Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence,
+//  such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, 
+// at least one of which is not in the default country.
+
+// function make_shirt(size:string, text:string){
+//     console.log(`Creating a ${size} shirt with the message: ${text}`)
+// }
+
+// make_shirt('large','Jump in fear');
+
+function c_ty(city:string, country:string = "Pakistan"){
+    console.log(`${city} is in ${country}`)
+
+}
+
+c_ty('Lahore');
+c_ty('Islamabad');
+c_ty("Perth","Australia");
---
+++ City Names3/city_names.ts
@@ -0,0 +1,6 @@
+function city_country(city: String,  country: string) {
+    return `${city},${country}`;
+}
+console.log (city_country('Karachi', 'Pakistan'));
+console.log (city_country('Lahore', 'Pakistan'));
+console.log (city_country('London', 'UK'));
