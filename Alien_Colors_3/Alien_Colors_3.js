de4009803d2af4be336b96225819a61608a14795
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 21:55:15 +0500

    Alien Colors #3
    : Turn your if-else chain from Exercise 5-4 into an if-else chain.
    • If the alien is green, print a message that the player earned 5 points.
    
    • If the alien is yellow, print a message that the player earned 10 points.
    
    • If the alien is red, print a message that the player earned 15 points.
    
    • Write three versions of this program, making sure each message is printed for the appropriate color alien.

---
+++ Alien_Colors_3/Alien_Colors_3.js
@@ -0,0 +1,43 @@
+"use strict";
+let aliencolors = "green";
+// If the alien is green, print a message that the player earned 5 points.
+if (aliencolors === "green") {
+    console.log("The player earned 5 point");
+}
+else if (aliencolors === "yellow") {
+    console.log("The player earned 10 point");
+}
+else if (aliencolors === "red") {
+    console.log("The player earned 15 point");
+}
+else {
+    console.log("Please select the right color");
+}
+// version 2
+aliencolors = "yellow";
+if (aliencolors === "green") {
+    console.log("The player earned 5 point");
+}
+else if (aliencolors === "yellow") {
+    console.log("The player earned 10 point");
+}
+else if (aliencolors === "red") {
+    console.log("The player earned 15 point");
+}
+else {
+    console.log("Please select the right color");
+}
+//  version 3
+aliencolors = "red";
+if (aliencolors === "green") {
+    console.log("The player earned 5 point");
+}
+else if (aliencolors === "yellow") {
+    console.log("The player earned 10 point");
+}
+else if (aliencolors === "red") {
+    console.log("The player earned 15 point");
+}
+else {
+    console.log("Please select the right color");
+}
