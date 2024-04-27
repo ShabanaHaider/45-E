7b52341655bb812dc906c5cf9a7a0ff92db6cbe5
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 23:52:15 +0500

    Pizzas
    Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.

--- City Names3/city_names.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function city_country(city, country) {
-    return `${city},${country}`;
-}
-console.log(city_country('Karachi', 'Pakistan'));
-console.log(city_country('Lahore', 'Pakistan'));
-console.log(city_country('London', 'UK'));
