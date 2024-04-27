66c59354ddaa707aca7d82e3441f6191df6490e5
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 22:01:02 +0500

    Hello Admin
    :Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
    • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
    
    • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.

---
+++ Hello Admin/dist/hello_admin.js
@@ -0,0 +1,15 @@
+"use strict";
+// Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+//  when if condition fail it will go on else condition
+// Round bracket is at last, then comes curley bracket and then square bracket
+const userNames = ['admin', 'Aamir', 'Asif', 'Bashir', 'Shabbir'];
+for (let i = 0; i < userNames.length; i++) {
+    if (userNames[i] === 'admin') {
+        console.log('Hello admin, would you like to see a status report?');
+    }
+    else {
+        console.log(`Hello ${userNames[i]}, thank you for logging in again`);
+    }
+}
