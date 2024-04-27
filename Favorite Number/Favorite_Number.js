83ec68eeb5998d79e24a2cdf9264a1cb1517a545
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 20:04:03 +0500

    Favorite Number
    Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ Favorite Number/Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favourite_number = 7;
+const message = 'This is my lucky number ' + favourite_number;
+console.log(message);
---
+++ Favorite Number/Favorite_Number.ts
@@ -0,0 +1,5 @@
+const favourite_number :number = 7 ;
+
+const message :string = 'This is my lucky number ' + favourite_number ;
+
+console.log(message) ;
