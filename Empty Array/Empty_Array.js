f3b7e31df15d6306c8c4f3f440327f7b2c4a4adc
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 20:34:55 +0500

    Empty Array
    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ Empty Array/Empty_Array.js
@@ -0,0 +1,10 @@
+"use strict";
+// Think of something you could store in a array.
+let items = ['Pakistan', 'Karachi', 'Urdu'];
+//  Write a program that creates a list containing these items.
+let item2 = [];
+//The array will store Germany on 1 index, Holland on 2 index and Italy on 3rd index
+item2.push('Germany');
+item2.push('Holland');
+item2.push('Italy');
+console.log(item2);
---
+++ Empty Array/Empty_Array.ts
@@ -0,0 +1,15 @@
+// Think of something you could store in a array.
+
+let items :string[] = ['Pakistan','Karachi','Urdu'];
+
+//  Write a program that creates a list containing these items.
+
+let item2 :string[] = [] ;
+//The array will store Germany on 1 index, Holland on 2 index and Italy on 3rd index
+item2.push('Germany');
+item2.push('Holland');
+item2.push('Italy');
+
+console.log(item2);
+
+
