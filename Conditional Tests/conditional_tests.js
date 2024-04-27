968870f32d95da83c4f0c03badc3a1d1696233bf
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 21:44:41 +0500

    Conditional Tests
    : Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
    
    let car = 'subaru';
    
    console.log("Is car == 'subaru'? I predict True.")
    
    console.log(car == 'subaru')
    
    • Look closely at your results, and make sure you understand why each line evaluates to True or False.
    
    • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.

---
+++ Conditional Tests/conditional_tests.js
@@ -0,0 +1,26 @@
+"use strict";
+//  Write a series of conditional tests
+//  creating car variable
+let car = "subaru";
+//  test no 1
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'subaru');
+// = means we are setting any value
+// == means we are checking any value
+//  === means means we are checking any value with its data type
+//  test no 2
+let flower = 'Jasmine';
+console.log("Is flower == 'Rose' ? I predict True.");
+console.log(car == 'Rose');
+// test no 3
+let aNimal = 'Elephant';
+console.log("Is aNimal == 'Elephant' ? I predict True.");
+console.log(aNimal == 'Elephant');
+// test no 4
+let cOuntry = 'Pakistan';
+console.log("Is cOuntry != 'Germany' ? I predict True.");
+console.log(cOuntry != 'Germany');
+// test no 5
+console.log("Is car.Upper case == 'SUBARU' ? I predict True");
+console.log(car.toUpperCase() == 'SUBARU');
+// != K matlab hota he not equal to
---
+++ Conditional Tests/conditional_tests.ts
@@ -0,0 +1,70 @@
+//  Write a series of conditional tests
+//  creating car variable
+let car :string = "subaru";
+
+//  test no 1
+console.log("Is car == 'subaru' ? I predict True.") ;
+console.log(car == 'subaru');
+
+
+// = means we are setting any value
+// == means we are checking any value
+//  === means means we are checking any value with its data type
+
+//  test no 2
+let flower :string = 'Jasmine';
+console.log ("Is flower == 'Rose' ? I predict True.") ;
+console.log(car == 'Rose');
+// test no 3
+
+let aNimal :string = 'Elephant';
+console.log ("Is aNimal == 'Elephant' ? I predict True.") ;
+console.log(aNimal == 'Elephant');
+// test no 4
+
+let cOuntry :string = 'Pakistan';
+console.log ("Is cOuntry != 'Germany' ? I predict True.") ;
+console.log(cOuntry != 'Germany');
+
+// test no 5
+console.log("Is car.Upper case == 'SUBARU' ? I predict True");
+console.log(car.toUpperCase() == 'SUBARU');
+
+// != K matlab hota he not equal to
+// test no 6
+console.log("Is car == 'SUBARU' ? I predict False");
+console.log(car == 'SUBARU');
+
+// test no 7
+console.log("Is car === 'SUBARU' ? I predict False");
+console.log(car === 'SUBARU');
+
+// test no 8
+console.log("Is car === 'Truck' ? I predict False");
+console.log(car === 'Truck');
+
+// test no 9
+console.log("Is car === 'Bus' ? I predict False");
+console.log(car === 'Bus');
+
+// test no 9
+console.log("Is car === 'Van' ? I predict False");
+console.log(car === 'Van');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
