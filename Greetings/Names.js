08b0dc577e54c54dca3be45f8a651b958f4b80ba
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-04-27 20:13:56 +0500

    Greetings
     Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Greetings/Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Shabana', 'Warda', 'Taha', 'Ali'];
+const message = 'My name is:';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Greetings/Names.ts
@@ -0,0 +1,10 @@
+
+
+let names :string[] = ['Shabana' , 'Warda' , 'Taha' , 'Ali' ] ;
+
+const message :string = 'My name is:'
+
+for(let i=0; i<names.length; i++) {
+
+    console.log(message + names[i]) ;
+}
